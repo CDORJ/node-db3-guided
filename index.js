19d4448d238ec00c8b637384622f6345d05fd92a
ChadDiaz <chadcdiaz@gmail.com>
2021-03-29 21:33:58 -0400

    set up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
     console.log(`\n** API running on port: ${PORT} **\n`);
